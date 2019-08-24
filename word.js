325f2accc0ecdf3f176290014a923ea57c91e534
David Ayres <dlayres@mymail.mines.edu>
2019-08-24 15:09:32 -0600

    mew

--- word.js
+++ word.js
@@ -5,11 +5,14 @@ let dictionaryHalf1;
 let dictionaryHalf2;
 let dictionary;
 let submitWordButton;
+let recallTilesButton;
 let userPos;
 let gridSpacing;
 let scrollFactor;
 let userTiles;
+let allTiles;
 let tilePossibilities;
+let tileWidth = 40;
 let canDrag = true;
 let draggingTile = false;
 let draggingTileIndex;
@@ -18,6 +21,13 @@ let dragOffsetY;
 let tileRestingPositions;
 let alertDebounce = 0;
 let debounceAmount = 15;
+let maxTiles = 7;
+let occupiedPositions;
+let committedPositions;
+let tileOrigX;
+let tileOrigY;
+let restingY;
+let bottomPanelHeight;
 
 function preload(){
   dictionaryHalf1 = loadStrings("https://gist.githubusercontent.com/dlayres/5919e00889614b854092b86d76d55815/raw/6025c962aaa62766140a9ea0bfadba9dd4d07e61/dictHalf1.txt");
@@ -30,11 +40,17 @@ function setup(){
   submitWordButton = createButton("Submit Word");
   submitWordButton.position(20, windowHeight - 30);
   submitWordButton.mousePressed(checkWord);
+  recallTilesButton = createButton("Recall Tiles");
+  recallTilesButton.position(20, windowHeight - 60);
+  recallTilesButton.mousePressed(recallTiles);
   userPos = createVector(0, 0);
   gridSpacing = 40;
+  bottomPanelHeight = 75;
   scrollFactor = 0.015;
 
   tileRestingPositions = [];
+  occupiedPositions = [];
+  committedPositions = [];
 
   // Tile letter distribution, blanks not yet implemented
   tilePossibilities = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
@@ -43,10 +59,15 @@ function setup(){
                        "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"];
 
   userTiles = [];
-  for(let i = 0; i < 7; i++){
-    tileRestingPositions.push(createVector(100 * (i + 2), windowHeight - 45));
+  allTiles = [];
+  restingY = windowHeight - bottomPanelHeight + ((bottomPanelHeight - tileWidth) / 2);
+  for(let i = 0; i < maxTiles; i++){
+    tileRestingPositions.push(createVector(50 * (i + 4), restingY));
     let tileLetter = tilePossibilities[Math.floor(Math.random() * tilePossibilities.length)];
-    userTiles.push(new Tile(tileLetter, tileRestingPositions[i].x, tileRestingPositions[i].y));
+    userTiles.push(new Tile(tileLetter, tileRestingPositions[i].x, tileRestingPositions[i].y, tileWidth));
+  }
+  for(let i = 0; i < maxTiles; i++){
+    allTiles.push(userTiles[i]);
   }
 }
 
@@ -65,15 +86,17 @@ function draw(){
 
   strokeWeight(2);
 
-  for(let i = 0; i < userTiles.length; i++){
-    userTiles[i].drawTile();
+  for(let i = 0; i < allTiles.length; i++){
+    allTiles[i].drawTile();
   }
 
-  rect(0, windowHeight - 50, windowWidth, windowHeight - 50);
+  rect(0, windowHeight - bottomPanelHeight, windowWidth, windowHeight - bottomPanelHeight);
   circle(userPos.x, userPos.y, 10);
 
   for(let i = 0; i < userTiles.length; i++){
-    if(!userTiles[i].onBoard){
+    if(!userTiles[i].onBoard && i != draggingTileIndex){
+      userTiles[i].x = tileRestingPositions[i].x;
+      userTiles[i].y = tileRestingPositions[i].y;
       userTiles[i].drawTile();
     }
   }
@@ -86,6 +109,7 @@ function draw(){
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
   submitWordButton.position(20, windowHeight - 30);
+  recallTilesButton.position(20, windowHeight - 60);
 }
 
 // Scrolling disabled until I figure out how to put tiles on a different-sized grid that looks good
@@ -103,22 +127,24 @@ function mouseDragged(event){
   }
   else if(canDrag){
     userPos.add(createVector(event.movementX, event.movementY));
-    for(let i = 0; i < userTiles.length; i++){
-      if(userTiles[i].onBoard){
-        userTiles[i].x += event.movementX;
-        userTiles[i].y += event.movementY;
+    for(let i = 0; i < allTiles.length; i++){
+      if(allTiles[i].onBoard){
+        allTiles[i].x += event.movementX;
+        allTiles[i].y += event.movementY;
       }
     }
   }
 }
 
 function mousePressed(){
-  if(mouseY > windowHeight - 50){
+  if(mouseY > windowHeight - bottomPanelHeight){
     canDrag = false;
   }
   for(let i = 0; i < userTiles.length; i++){
-    if(!userTiles[i].committedToBoard && mouseX > userTiles[i].x && mouseX < userTiles[i].x + userTiles[i].width && mouseY > userTiles[i].y && mouseY < userTiles[i].y + userTiles[i].width){
+    if(!userTiles[i].committedToBoard && mouseX > userTiles[i].x && mouseX < userTiles[i].x + tileWidth && mouseY > userTiles[i].y && mouseY < userTiles[i].y + tileWidth){
       draggingTile = true;
+      tileOrigX = userTiles[i].x;
+      tileOrigY = userTiles[i].y;
       draggingTileIndex = i;
       dragOffsetX = mouseX - userTiles[i].x;
       dragOffsetY = mouseY - userTiles[i].y;
@@ -136,22 +162,64 @@ function mousePressed(){
 function mouseReleased(){
   canDrag = true;
 
+  let tileBoardX;
+  let tileBoardY;
+
   if(draggingTile){
-    let tileWidth = userTiles[draggingTileIndex].width;
-    let tileScreenCenter = createVector(userTiles[draggingTileIndex].x + tileWidth / 2, userTiles[draggingTileIndex].y + tileWidth  / 2);
+    let tileScreenCenter = createVector(userTiles[draggingTileIndex].x + tileWidth / 2, userTiles[draggingTileIndex].y + tileWidth / 2);
     let tileOffsetCenter = p5.Vector.sub(tileScreenCenter, userPos);
 
-    if(tileScreenCenter.y > windowHeight - 50){
+    let ableToPlace = true;
+    if(tileScreenCenter.y > windowHeight - bottomPanelHeight){
+      ableToPlace = false;
+    }
+    else{
+      tileBoardX = tileOffsetCenter.x - tileOffsetCenter.x % 10;
+      tileBoardY = tileOffsetCenter.y - tileOffsetCenter.y % 10;
+      while(tileBoardX % gridSpacing != 0){
+        tileBoardX -= 10;
+      }
+      while(tileBoardY % gridSpacing != 0){
+        tileBoardY -= 10;
+      }
+      tileBoardX /= gridSpacing;
+      tileBoardY /= gridSpacing;
+
+      for(let i = 0; i < occupiedPositions.length; i++){
+        if(occupiedPositions[i].x == tileBoardX && occupiedPositions[i].y == tileBoardY){
+          ableToPlace = false;
+          break;
+        }
+      }
+    }
+
+
+    if(!ableToPlace){
       draggingTile = false;
       userTiles[draggingTileIndex].x = tileRestingPositions[draggingTileIndex].x;
       userTiles[draggingTileIndex].y = tileRestingPositions[draggingTileIndex].y;
       userTiles[draggingTileIndex].onBoard = false;
+
+      tileBoardX = userTiles[draggingTileIndex].boardX;
+      tileBoardY = userTiles[draggingTileIndex].boardY;
+
+      let posIndex = -1;
+      for(let i = 0; i < occupiedPositions.length; i++){
+        if(occupiedPositions[i].x == tileBoardX && occupiedPositions[i].y == tileBoardY){
+          posIndex = i;
+          break;
+        }
+      }
+      if(posIndex != -1){
+        occupiedPositions.splice(posIndex, 1);
+      }
+
+      userTiles[draggingTileIndex].boardX = -1;
+      userTiles[draggingTileIndex].boardY = -1;
     }
     else{
-      checkTileX = tileOffsetCenter.x;
-      checkTileY = tileOffsetCenter.y;
-      floorX = checkTileX - checkTileX % 10;
-      floorY = checkTileY - checkTileY % 10;
+      floorX = tileOffsetCenter.x - tileOffsetCenter.x % 10;
+      floorY = tileOffsetCenter.y - tileOffsetCenter.y % 10;
       while(floorX % gridSpacing != 0){
         floorX -= 10;
       }
@@ -159,23 +227,78 @@ function mouseReleased(){
         floorY -= 10;
       }
 
+      if(userTiles[draggingTileIndex].onBoard){
+        let tileBoardX = userTiles[draggingTileIndex].boardX;
+        let tileBoardY = userTiles[draggingTileIndex].boardY;
+
+        let posIndex = -1;
+        for(let i = 0; i < occupiedPositions.length; i++){
+          if(occupiedPositions[i].x == tileBoardX && occupiedPositions[i].y == tileBoardY){
+            posIndex = i;
+            break;
+          }
+        }
+        if(posIndex != -1){
+          occupiedPositions.splice(posIndex, 1);
+        }
+      }
+
       userTiles[draggingTileIndex].x = floorX + userPos.x;
       userTiles[draggingTileIndex].y = floorY + userPos.y;
       userTiles[draggingTileIndex].onBoard = true;
+      userTiles[draggingTileIndex].boardX = floorX / gridSpacing;
+      userTiles[draggingTileIndex].boardY = floorY / gridSpacing;
+
+      occupiedPositions.push(createVector(floorX / gridSpacing, floorY / gridSpacing));
     }
     draggingTile = false;
   }
+  draggingTileIndex = -1;
 }
 
+function recallTiles(){
+  for(let i = 0; i < userTiles.length; i++){
+    if(userTiles[i].onBoard && !userTiles[i].committedToBoard){
+      userTiles[i].x = tileRestingPositions[i].x;
+      userTiles[i].y = tileRestingPositions[i].y;
+      userTiles[i].onBoard = false;
+
+      tileBoardX = userTiles[i].boardX;
+      tileBoardY = userTiles[i].boardY;
+
+      let posIndex = -1;
+      for(let j = 0; j < occupiedPositions.length; j++){
+        if(occupiedPositions[j].x == tileBoardX && occupiedPositions[j].y == tileBoardY){
+          posIndex = j;
+          break;
+        }
+      }
+      if(posIndex != -1){
+        occupiedPositions.splice(posIndex, 1);
+      }
+
+      userTiles[i].boardX = -1;
+      userTiles[i].boardY = -1;
+    }
+  }
+}
 
 function checkWord(){
   // tiles on board
   let tilesOnBoard = [];
   let tileIndices = [];
+  let wordTiles = [];
+  // all x positions
+  // all y positions
+  let allX = [];
+  let allY = [];
   for(let i = 0; i < userTiles.length; i++){
     if(userTiles[i].onBoard && !userTiles[i].committedToBoard){
       tilesOnBoard.push(userTiles[i]);
       tileIndices.push(i);
+      wordTiles.push(userTiles[i]);
+      allX.push(userTiles[i].boardX);
+      allY.push(userTiles[i].boardY);
     }
   }
 
@@ -190,76 +313,248 @@ function checkWord(){
   let validHorizontal = true;
   // first check if all on same y position
   let tileY = tilesOnBoard[0].y;
+  let tileBoardY = allY[0];
   for(let i = 0; i < tilesOnBoard.length; i++){
     if(tilesOnBoard[i].y != tileY){
       validHorizontal = false;
     }
   }
   if(validHorizontal){
-    // still plausible, check adjacency
-    for(let i = 0; i < tilesOnBoard.length - 1; i++){
-      if(tilesOnBoard[i + 1].x - tilesOnBoard[i].x != gridSpacing){
-        validHorizontal = false;
-      }
-    }
-  }
-  if(validHorizontal){
-    // definitely horizontal, construct word
-    word = "";
+    // still plausible, check adjacency, along with preexisting tiles
+    // find rightmost user tile (... spreads array into list)
+    let rightmostX = Math.max(...allX);
+    let storedX = rightmostX;
+    // find index of tile and remove it
     for(let i = 0; i < tilesOnBoard.length; i++){
-      word += tilesOnBoard[i].letter;
-    }
-    // submit word
-    if(checkDictionary(word)){
-      for(let i = 0; i < tileIndices.length; i++){
-        userTiles[tileIndices[i]].committedToBoard = true;
+      if(tilesOnBoard[i].boardX == rightmostX){
+        tilesOnBoard.splice(i, 1);
+        break;
       }
-      sendAlert(word.toUpperCase() + " played successfully.")
+    }
+    // assume a preexisting tile is directly to its right, keep going until one isn't
+    while(true){
+      let foundTile = false;
+      for(let i = 0; i < committedPositions.length; i++){
+        if(committedPositions[i].y == tileBoardY && committedPositions[i].x == rightmostX + 1){
+          // preexisting tile does exist there, find it in list of tiles
+          let checkX = rightmostX + 1;
+          let checkY = tileBoardY;
+          for(let j = 0; j < allTiles.length; j++){
+            if(allTiles[j].boardX == checkX && allTiles[j].boardY == checkY){
+              wordTiles.push(allTiles[j]);
+            }
+          }
+          rightmostX += 1;
+          foundTile = true;
+          break;
+        }
+      }
+      if(!foundTile){
+        break;
+      }
+    }
+    // keep going left from storedX (original rightmostX) until encounter empty space
+    let foundTile = true;
+    while(foundTile){
+      storedX -= 1;
+      // look in user tiles
+      let foundUserTile = false;
+      for(let i = 0; i < tilesOnBoard.length; i++){
+        if(tilesOnBoard[i].boardX == storedX){
+          tilesOnBoard.splice(i, 1);
+          foundUserTile = true;
+          break;
+        }
+      }
+
+      // look in preexisting tiles
+      if(!foundUserTile){
+        for(let i = 0; i < committedPositions.length; i++){
+          if(committedPositions[i].y == tileBoardY && committedPositions[i].x == storedX){
+            foundUserTile = true;
+            // find tile in list of tiles
+            for(let j = 0; j < allTiles.length; j++){
+              if(allTiles[j].boardX == storedX && allTiles[j].boardY == tileBoardY){
+                wordTiles.push(allTiles[j]);
+                break;
+              }
+            }
+            break;
+          }
+        }
+      }
+
+      if(!foundUserTile){
+        foundTile = false;
+      }
+    }
+
+    // check if tiles are still on board, if so, invalid configuration
+    if(tilesOnBoard.length > 0){
+      validHorizontal = false;
     }
     else{
-      sendAlert(word.toUpperCase() + " is not a valid word.");
+      // definitely horizontal, construct word
+      wordTiles.sort((a, b) => (a.x > b.x) ? 1 : -1);
+      word = "";
+      for(let i = 0; i < wordTiles.length; i++){
+        word += wordTiles[i].letter;
+      }
+      // submit word
+      if(checkDictionary(word)){
+        for(let i = 0; i < tileIndices.length; i++){
+          userTiles[tileIndices[i]].committedToBoard = true;
+          let tileLetter = tilePossibilities[Math.floor(Math.random() * tilePossibilities.length)];
+          userTiles[tileIndices[i]] = new Tile(tileLetter, tileRestingPositions[i].x, tileRestingPositions[i].y, tileWidth);
+          allTiles.push(userTiles[tileIndices[i]]);
+        }
+        for(let i = 0; i < occupiedPositions.length; i++){
+          committedPositions.push(occupiedPositions[i]);
+        }
+        sendAlert(word.toUpperCase() + " played successfully.")
+      }
+      else{
+        sendAlert(word.toUpperCase() + " is not a valid word.");
+      }
+      return;
     }
-    return;
+  }
+
+  // tiles on board
+  tilesOnBoard = [];
+  tileIndices = [];
+  wordTiles = [];
+  // all x positions
+  // all y positions
+  allX = [];
+  allY = [];
+  for(let i = 0; i < userTiles.length; i++){
+    if(userTiles[i].onBoard && !userTiles[i].committedToBoard){
+      tilesOnBoard.push(userTiles[i]);
+      tileIndices.push(i);
+      wordTiles.push(userTiles[i]);
+      allX.push(userTiles[i].boardX);
+      allY.push(userTiles[i].boardY);
+    }
   }
 
   tilesOnBoard.sort((a, b) => (a.y > b.y) ? 1 : -1);
+
   // check vertical plausibility
   let validVertical = true;
   // first check if all on same x position
   let tileX = tilesOnBoard[0].x;
+  let tileBoardX = allX[0];
   for(let i = 0; i < tilesOnBoard.length; i++){
     if(tilesOnBoard[i].x != tileX){
       validVertical = false;
     }
   }
   if(validVertical){
-    // still plausible, check adjacency
-    for(let i = 0; i < tilesOnBoard.length - 1; i++){
-      if(tilesOnBoard[i + 1].y - tilesOnBoard[i].y != gridSpacing){
-        validVertical = false;
-      }
-    }
-  }
-  if(validVertical){
-    // definitely vertical, construct word
-    word = "";
+    // still plausible, check adjacency, along with preexisting tiles
+    // find bottommost user tile (... spreads array into list)
+    let bottommostY = Math.max(...allY);
+    let storedY = bottommostY;
+    // find index of tile and remove it
     for(let i = 0; i < tilesOnBoard.length; i++){
-      word += tilesOnBoard[i].letter;
-    }
-    // submit word
-    if(checkDictionary(word)){
-      for(let i = 0; i < tileIndices.length; i++){
-        userTiles[tileIndices[i]].committedToBoard = true;
+      if(tilesOnBoard[i].boardY == bottommostY){
+        tilesOnBoard.splice(i, 1);
+        break;
       }
-      sendAlert(word.toUpperCase() + " played successfully.")
+    }
+    // assume a preexisting tile is directly underneath it, keep going until one isn't
+    while(true){
+      let foundTile = false;
+      for(let i = 0; i < committedPositions.length; i++){
+        if(committedPositions[i].x == tileBoardX && committedPositions[i].y == bottommostY + 1){
+          // preexisting tile does exist there, find it in list of tiles
+          let checkY = bottommostY + 1;
+          let checkX = tileBoardX;
+          for(let j = 0; j < allTiles.length; j++){
+            if(allTiles[j].boardX == checkX && allTiles[j].boardY == checkY){
+              wordTiles.push(allTiles[j]);
+            }
+          }
+          bottommostY += 1;
+          foundTile = true;
+          break;
+        }
+      }
+      if(!foundTile){
+        break;
+      }
+    }
+    // keep going up from storedY (original bottommostY) until encounter empty space
+    let foundTile = true;
+    while(foundTile){
+      storedY -= 1;
+      // look in user tiles
+      let foundUserTile = false;
+      for(let i = 0; i < tilesOnBoard.length; i++){
+        if(tilesOnBoard[i].boardY == storedY){
+          tilesOnBoard.splice(i, 1);
+          foundUserTile = true;
+          break;
+        }
+      }
+
+      // look in preexisting tiles
+      if(!foundUserTile){
+        for(let i = 0; i < committedPositions.length; i++){
+          if(committedPositions[i].x == tileBoardX && committedPositions[i].y == storedY){
+            foundUserTile = true;
+            // find tile in list of tiles
+            for(let j = 0; j < allTiles.length; j++){
+              if(allTiles[j].boardY == storedY && allTiles[j].boardX == tileBoardX){
+                wordTiles.push(allTiles[j]);
+                break;
+              }
+            }
+            break;
+          }
+        }
+      }
+
+      if(!foundUserTile){
+        foundTile = false;
+      }
+    }
+
+    // check if tiles are still on board, if so, invalid configuration
+    if(tilesOnBoard.length > 0){
+      validVertical = false;
     }
     else{
-      sendAlert(word.toUpperCase() + " is not a valid word.");
+      // definitely vertical, construct word
+      wordTiles.sort((a, b) => (a.y > b.y) ? 1 : -1);
+      word = "";
+      for(let i = 0; i < wordTiles.length; i++){
+        word += wordTiles[i].letter;
+      }
+      // submit word
+      if(checkDictionary(word)){
+        for(let i = 0; i < tileIndices.length; i++){
+          userTiles[tileIndices[i]].committedToBoard = true;
+          let tileLetter = tilePossibilities[Math.floor(Math.random() * tilePossibilities.length)];
+          userTiles[tileIndices[i]] = new Tile(tileLetter, tileRestingPositions[i].x, tileRestingPositions[i].y, tileWidth);
+          allTiles.push(userTiles[tileIndices[i]]);
+        }
+        for(let i = 0; i < occupiedPositions.length; i++){
+          committedPositions.push(occupiedPositions[i]);
+        }
+        sendAlert(word.toUpperCase() + " played successfully.")
+      }
+      else{
+        sendAlert(word.toUpperCase() + " is not a valid word.");
+      }
+      return;
     }
-    return;
   }
 
-  sendAlert("Your tiles are not in a valid configuration.");
+  if(!validHorizontal && !validVertical){
+    sendAlert("Your tiles are not in a valid configuration.");
+    return;
+  }
 }
 
 
